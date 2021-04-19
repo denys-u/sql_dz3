d9e2a411da6c10214684cc7b75ce438d27802128
denys-u <denys.udovik@nure.ua>
2021-04-19 18:17:46 +0300

    created proj

---
+++ sql_dz3/sql_dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sql_dz3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
